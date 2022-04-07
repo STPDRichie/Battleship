1a5a50691a251c1dd77dfa3928477d94d0113213
Коляска _ <nikolaj.karpov.2002@mail.ru>
2022-04-07 16:55:51 +0500

    ai-board added. game-panel added

--- Battleship/static/js/script.js
+++ Battleship/static/js/script.js
@@ -1,19 +1,53 @@
+var game_status_text = document.getElementById('game_status');
+game_status_text.addEventListener('click', function() {
+  if (game_status_text.innerHTML == 'Start game') {
+    game_status_text.innerHTML = 'Place your ships';
+  }
+  else if (game_status_text.innerHTML == 'Place your ships') {
+    game_status_text.innerHTML = 'Start game';
+  }
+});
+
 var solid = '<i class="fa-solid"></i>';
 var circle = '<i class="fa-solid fa-circle"></i>';
 var crossedCircle = '<i class="fa-solid fa-circle-xmark"></i>';
 
-var cells = document.getElementsByClassName('board_cell');
+var player_cells = document.getElementById('player-board').getElementsByClassName('board_cell');
+var ai_cells = document.getElementById('ai-board').getElementsByClassName('board_cell');
 
-Array.prototype.forEach.call(cells, function(element) {
+Array.prototype.forEach.call(player_cells, function(element) {
 	element.addEventListener('click', function() {
-      if (element.innerHTML == solid) {
-        element.innerHTML = circle;
-      }
-      else if (element.innerHTML == circle) {
-        element.innerHTML = crossedCircle;
-      }
-      else if (element.innerHTML == crossedCircle) {
-        element.innerHTML = solid;
-      }
+    handlePlayerBoard(element);
 	});
 });
+
+Array.prototype.forEach.call(ai_cells, function(element) {
+  element.addEventListener('click', function() {
+    handleAIBoard(element);
+  });
+});
+
+function handlePlayerBoard(cell) {
+  if (game_status_text.innerHTML != 'Start game') {
+    if (cell.innerHTML == solid) {
+      cell.innerHTML = circle;
+    }
+    else if (cell.innerHTML == circle) {
+      cell.innerHTML = crossedCircle;
+    }
+    else if (cell.innerHTML == crossedCircle) {
+      cell.innerHTML = solid;
+    }
+  }
+}
+
+function handleAIBoard(cell) {
+  if (game_status_text.innerHTML == 'Place your ships') {
+    return;
+  }
+  if (game_status_text.innerHTML != 'Start game') {
+    if (cell.innerHTML == solid || cell.innerHTML == circle) {
+      cell.innerHTML = crossedCircle;
+    }
+  }
+}
