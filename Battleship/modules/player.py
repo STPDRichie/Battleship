c874b255be037bd55f56170f04ccd8c3832940fd
Коляска _ <nikolaj.karpov.2002@mail.ru>
2022-05-10 20:42:55 +0500

    code reformat

--- Battleship/modules/player.py
+++ Battleship/modules/player.py
@@ -1,7 +1,6 @@
 from modules.domain import GameStatus, CellStatus, ShipDirection
 from modules.ship import Ship
 
-
 ships_ranges = {
     'Battleship': [-1, 2],
     'Cruiser': [-1, 1],
@@ -19,7 +18,7 @@ def get_ship_direction(ship_cells):
 def get_ship_neighbor_cells(ship_cells):
     ship_direction = get_ship_direction(ship_cells)
     neighbors = []
-
+    
     if ship_direction == ShipDirection.VERTICAL.value:
         start_row = ship_cells[0][0] - 1
         center_column = ship_cells[0][1]
@@ -31,7 +30,7 @@ def get_ship_neighbor_cells(ship_cells):
                     neighbors.append((r, center_column - 1))
                 if center_column + 1 <= 9:
                     neighbors.append((r, center_column + 1))
-
+    
     if ship_direction == ShipDirection.HORIZONTAL.value:
         start_column = ship_cells[0][1] - 1
         center_row = ship_cells[0][0]
@@ -43,24 +42,24 @@ def get_ship_neighbor_cells(ship_cells):
                     neighbors.append((center_row - 1, c))
                 if center_row + 1 <= 9:
                     neighbors.append((center_row + 1, c))
-
+    
     return neighbors
 
 
 def get_ship_cells(cell, ship_name, ship_direction):
     cells = []
     ship_range = ships_ranges[ship_name]
-
+    
     if ship_direction == ShipDirection.VERTICAL.value:
         for r in range(ship_range[0], ship_range[1] + 1):
             if 0 <= cell[0] + r <= 9:
                 cells.append((cell[0] + r, cell[1]))
-
+    
     if ship_direction == ShipDirection.HORIZONTAL.value:
         for c in range(ship_range[0], ship_range[1] + 1):
             if 0 <= cell[1] + c <= 9:
                 cells.append((cell[0], cell[1] + c))
-
+    
     return cells
 
 
@@ -70,7 +69,7 @@ class Player:
             [[CellStatus.EMPTY.value for _ in range(10)] for _ in range(10)]
         self.neighbors_board = \
             [[0 for _ in range(10)] for _ in range(10)]
-
+        
         self.non_placed_ships_count = 10
         self.ships_remains_to_place = {
             'Battleship': 1,
@@ -78,79 +77,79 @@ class Player:
             'Submarine': 3,
             'Destroyer': 4
         }
-
+        
         self.ships = []
-
+        
         self.opponent = None
-
+        
         self.remaining_ship_cells_count = 20
         self.opponent_remaining_ship_cells_count = 20
         self.destroyed_ship_cells = []
-
+    
     def init_opponent(self, opponent):
         self.opponent = opponent
-
+    
     def fire(self, cell):
         fired_cell_status, is_one_more = self.opponent.get_fired(cell)
-
+        
         if is_one_more:
             self.opponent_remaining_ship_cells_count -= 1
-
+        
         return fired_cell_status
-
+    
     def get_fired(self, cell):
         row, column = cell[0], cell[1]
         current_cell = self.board[row][column]
-
+        
         fired_cell_status = CellStatus.MISFIRE.value
         is_one_more = False
-
+        
         if current_cell == CellStatus.DESTROYED.value:
             fired_cell_status = CellStatus.DESTROYED.value
-
+        
         if current_cell in (CellStatus.EMPTY.value,
                             CellStatus.NEIGHBOR.value):
             self.board[row][column] = CellStatus.MISFIRE.value
-
+        
         if current_cell == CellStatus.SHIP.value:
             self.destroyed_ship_cells.append((row, column))
             fired_cell_status = CellStatus.DESTROYED.value
             self.board[row][column] = CellStatus.DESTROYED.value
             self.remaining_ship_cells_count -= 1
             is_one_more = True
-
+        
         return fired_cell_status, is_one_more
-
+    
     def place_ship(self, cell, ship_name, ship_direction):
         if not self.is_placement_correct(cell, ship_name, ship_direction):
             return []
-
+        
         ship_cells = get_ship_cells(cell, ship_name, ship_direction)
         neighbor_cells = get_ship_neighbor_cells(ship_cells)
-
+        
         for nbr_cell in neighbor_cells:
             self.board[nbr_cell[0]][nbr_cell[1]] = CellStatus.NEIGHBOR.value
         for ship_cell in ship_cells:
             self.board[ship_cell[0]][ship_cell[1]] = CellStatus.SHIP.value
         for nbr_cell in neighbor_cells:
             self.neighbors_board[nbr_cell[0]][nbr_cell[1]] += 1
-
+        
         self.non_placed_ships_count -= 1
         self.ships_remains_to_place[ship_name] -= 1
-
+        
         ship = Ship(ship_cells, neighbor_cells, ship_name, ship_direction)
         self.ships.append(ship)
-
+        
         return ship_cells
-
+    
     def is_placement_correct(self, cell, ship_name, ship_direction):
         if self.ships_remains_to_place[ship_name] == 0:
             return False
-
+        
         row = cell[0]
         column = cell[1]
         ship_range = ships_ranges[ship_name]
-
+        
         if ship_direction == ShipDirection.VERTICAL.value:
             if row + ship_range[0] < 0 or row + ship_range[1] > 9:
                 return False
@@ -159,7 +158,7 @@ class Player:
                         (CellStatus.NEIGHBOR.value,
                          CellStatus.SHIP.value):
                     return False
-
+        
         if ship_direction == ShipDirection.HORIZONTAL.value:
             if column + ship_range[0] < 0 or column + ship_range[1] > 9:
                 return False
@@ -168,12 +167,12 @@ class Player:
                         (CellStatus.NEIGHBOR.value,
                          CellStatus.SHIP.value):
                     return False
-
+        
         return True
-
+    
     def uninit_and_get_ship_cells(self, cell):
         self.non_placed_ships_count += 1
-
+        
         for ship in self.ships:
             for ship_cell in ship.cells:
                 if ship_cell == cell:
@@ -183,11 +182,11 @@ class Player:
                     self._uninit_ship(ship)
                     return cells
         return None
-
+    
     def _uninit_ship(self, ship):
         ship_range = ships_ranges[ship.name]
         reset_length = abs(ship_range[0]) + ship_range[1] + 3
-
+        
         if ship.direction == ShipDirection.VERTICAL.value:
             start_r = ship.cells[0][0] - 1
             center_c = ship.cells[0][1]
@@ -199,7 +198,7 @@ class Player:
                         self._uninit_cell(r, center_c - 1)
                     if center_c + 1 <= 9:
                         self._uninit_cell(r, center_c + 1)
-
+        
         if ship.direction == ShipDirection.HORIZONTAL.value:
             start_c = ship.cells[0][1] - 1
             center_r = ship.cells[0][0]
@@ -211,57 +210,58 @@ class Player:
                         self._uninit_cell(center_r - 1, c)
                     if center_r + 1 <= 9:
                         self._uninit_cell(center_r + 1, c)
-
+    
     def _uninit_cell(self, row, column):
         self.neighbors_board[row][column] -= 1
         if self.board[row][column] == CellStatus.SHIP.value or \
                 self.neighbors_board[row][column] == 0:
             self.board[row][column] = CellStatus.EMPTY.value
-
+    
     def check_game_status(self):
-        if self.non_placed_ships_count > 0:
+        if self.non_placed_ships_count > 0 or \
+                self.opponent.non_placed_ships_count > 0:
             return GameStatus.PLACE_SHIPS.value
-
+        
         if self.opponent_remaining_ship_cells_count == 0:
             return GameStatus.WIN.value
-
+        
         if self.remaining_ship_cells_count == 0:
             return GameStatus.LOSE.value
-
+        
         return GameStatus.BATTLE.value
-
+    
     def get_remains_to_place_ship_count(self, ship_name):
         return self.ships_remains_to_place[ship_name]
-
+    
     def get_ship_name(self, cell):
         for ship in self.ships:
             for ship_cell in ship.cells:
                 if ship_cell == cell:
                     return ship.name
         return None
-
+    
     def is_ship_destroyed(self, cell):
         ship_cells = []
         for ship in self.ships:
             for ship_cell in ship.cells:
                 if ship_cell == cell:
                     ship_cells = ship.cells
-
+        
         if not ship_cells:
             return False
-
+        
         for ship_cell in ship_cells:
             if self.board[ship_cell[0]][ship_cell[1]] == \
                     CellStatus.SHIP.value:
                 return False
-
+        
         return True
-
+    
     def get_remaining_ship_cells(self):
         remaining_cells = []
         for ship in self.ships:
             for ship_cell in ship.cells:
                 if ship_cell not in self.destroyed_ship_cells:
                     remaining_cells.append(ship_cell)
-
+        
         return remaining_cells
--- Battleship/modules/robot.py
+++ Battleship/modules/robot.py
@@ -13,25 +13,25 @@ class Robot(Player):
     def __init__(self):
         super().__init__()
         self.opponent_not_empty_cells = set()
-
+        
         self.next_cells_to_fire = []
         self.last_destroyed_cell = None
-
+    
     def init_board(self):
         while self.non_placed_ships_count > 0:
             ship = ship_list[randint(0, 3)]
             while self.ships_remains_to_place[ship] == 0:
                 ship = ship_list[randint(0, 3)]
-
+            
             row, column = randint(0, 9), randint(0, 9)
             ship_direction = directions[randint(0, 1)]
             while not self.is_placement_correct((row, column),
                                                 ship, ship_direction):
                 row, column = randint(0, 9), randint(0, 9)
                 ship_direction = directions[randint(0, 1)]
-
+            
             self.place_ship((row, column), ship, ship_direction)
-
+    
     def random_fire(self):
         next_cells_length = len(self.next_cells_to_fire)
         if next_cells_length != 0:
@@ -41,24 +41,24 @@ class Robot(Player):
             cell = (randint(0, 9), randint(0, 9))
             while cell in self.opponent_not_empty_cells:
                 cell = (randint(0, 9), randint(0, 9))
-
+        
         fired_cell_status, is_one_more = self.opponent.get_fired(cell)
         self.opponent_not_empty_cells.add(cell)
-
+        
         if fired_cell_status == CellStatus.DESTROYED.value:
             self.update_cells_to_fire_by_destroyed(cell)
             self.last_destroyed_cell = cell
             if is_one_more:
                 self.opponent_remaining_ship_cells_count -= 1
-
+        
         if self.opponent.is_ship_destroyed(cell):
             for next_cell in self.next_cells_to_fire:
                 self.opponent_not_empty_cells.add(next_cell)
             self.next_cells_to_fire.clear()
             self.last_destroyed_cell = None
-
+        
         return cell, fired_cell_status
-
+    
     def update_cells_to_fire_by_destroyed(self, cell):
         cell_neighbors = []
         for row in range(-1, 2):
@@ -70,34 +70,34 @@ class Robot(Player):
                     else:
                         self.opponent_not_empty_cells.add(
                             (row + cell[0], column + cell[1]))
-
+        
         next_cells = self.get_empty_cells_from_neighbors(cell_neighbors)
         self.next_cells_to_fire.extend(next_cells)
         self.refine_next_cells_to_fire(cell)
-
+    
     def get_empty_cells_from_neighbors(self, neighbors):
         cells = []
         for cell in neighbors:
             if cell not in self.opponent_not_empty_cells:
                 cells.append(cell)
-
+        
         return cells
-
+    
     def refine_next_cells_to_fire(self, cell):
         if not self.last_destroyed_cell:
             return
-
+        
         if cell[0] == self.last_destroyed_cell[0]:
             self._refine_next_cells_by_direction(cell, 0)
         else:
             self._refine_next_cells_by_direction(cell, 1)
-
+    
     def _refine_next_cells_by_direction(self, cell, dir_index):
         next_cells = self.next_cells_to_fire.copy()
         for next_cell in next_cells:
             if next_cell == self.last_destroyed_cell:
                 self.next_cells_to_fire.remove(next_cell)
-
+            
             if next_cell[dir_index] != cell[dir_index]:
                 self.next_cells_to_fire.remove(next_cell)
                 self.opponent_not_empty_cells.add(next_cell)
